b2eacbb0f06163ea923ea5373d85833c61e9511f
Rajdeep Das <das.rajdeep97@gmail.com>
2013-12-23 18:34:37 +0530

    removed example files.

--- app/helpers/exampleHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getCurrentTime() {
-	
-	return time();
-}
-
-?>
--- app/views/main/front.php
+++
@@ -1,6 +0,0 @@
-<?php Helper::set_complete_view(); ?>
-<?php Helper::add_dependancy('front.css'); ?>
-
-<h1>This is a view</h1>
-<?php Helper::add_view_component('component'); ?>
-Generated at <?php echo Helper::getCurrentTime(); ?>
