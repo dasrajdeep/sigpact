a03d01e6f2ad0caf51210e7f789493644788a795
Rajdeep Das <das.rajdeep97@gmail.com>
2014-06-15 23:47:16 +0530

    local clone created.

--- app/environment.php
+++ app/environment.php
@@ -42,7 +42,7 @@
 	 * your domain, then you must change this value to the 
 	 * appropriate name of the sub-directory.
 	 */
-	$env_base_uri 				 = 'http://stage8.x64.me/researchers/';
+	$env_base_uri 				 = 'http://stage8.x64.me/sigpact/';
 	
 	/**
 	 * Set the type of CDN to be used by your app.
